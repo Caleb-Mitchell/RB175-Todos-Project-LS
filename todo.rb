b8712fb0a06cf81d032c5fd1252f5eda59b338f7
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-10-10 16:58:16 -0500

    Continue todos project, adding comments and flash messages

--- todo.rb
+++ todo.rb
@@ -15,16 +15,26 @@ get "/" do
   redirect "/lists"
 end
 
+# View list of lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  redirect "/lists"
+  list_name = params[:list_name]
+  if list_name.size >= 1 && list_name.size <= 100
+    session[:lists] << { name: params[:list_name], todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  else
+    session[:error] = "List name must bebetween 1 and 100 characters."
+    erb :new_list, layout: :layout
+  end
 end
