e4511c148376555a64fb322a7d1484fdcb53f446
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-10-10 20:12:36 -0500

    Continue todos project, adding `content_for` blocks

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
 
 configure do
@@ -26,15 +27,32 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must bebetween 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
+end
+
 # Create a new list
 post "/lists" do
-  list_name = params[:list_name]
-  if list_name.size >= 1 && list_name.size <= 100
-    session[:lists] << { name: params[:list_name], todos: [] }
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
     redirect "/lists"
-  else
-    session[:error] = "List name must bebetween 1 and 100 characters."
-    erb :new_list, layout: :layout
   end
+end
+
+get "/lists/:id" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :list, layout: :layout
 end
