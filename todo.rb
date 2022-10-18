089fd5a5ffbfb579b8e23db03a56df23c1bdf857
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-10-18 14:56:17 -0500

    Add list id validation, and disable rubocop for block length of helpers block

--- todo.rb
+++ todo.rb
@@ -12,6 +12,7 @@ before do
   session[:lists] ||= []
 end
 
+# rubocop:disable Metrics/BlockLength
 helpers do
   def list_complete?(list)
     todos_remaining_count(list).zero? && !todos_count(list).zero?
@@ -30,18 +31,50 @@ helpers do
   end
 
   def sort_lists(lists)
-    complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
+    complete_lists, incomplete_lists = lists.partition do |list|
+      list_complete?(list)
+    end
 
     incomplete_lists.each { |list| yield list, lists.index(list) }
     complete_lists.each { |list| yield list, lists.index(list) }
   end
 
   def sort_todos(todos)
-    complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
+    complete_todos, incomplete_todos = todos.partition do |todo|
+      todo[:completed]
+    end
 
-    incomplete_todos.each { |todo| yield todo, todos.index(todo) }
-    complete_todos.each { |todo| yield todo, todos.index(todo) }
+    incomplete_todos.each do |todo|
+      yield todo, todos.index(todo)
+    end
+    complete_todos.each do |todo|
+      yield todo, todos.index(todo)
+    end
   end
+end
+# rubocop:enable Metrics/BlockLength
+
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
+end
+
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_todo(name)
+  "Todo must be between 1 and 100 characters." unless (1..100).cover? name.size
+end
+
+# Validate that requested list id exists and is valid
+def load_list(index)
+  list = session[:lists][index] if index && session[:lists][index]
+  return list if list
+
+  session[:error] = "The specified list was not found."
+  redirect "/lists"
 end
 
 get "/" do
@@ -57,22 +90,6 @@ end
 # Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
-end
-
-# Return an error message if the name is invalid. Return nil if name is valid.
-def error_for_list_name(name)
-  if !(1..100).cover? name.size
-    "List name must bebetween 1 and 100 characters."
-  elsif session[:lists].any? { |list| list[:name] == name }
-    "List name must be unique."
-  end
-end
-
-# Return an error message if the name is invalid. Return nil if name is valid.
-def error_for_todo(name)
-  if !(1..100).cover? name.size
-    "Todo must bebetween 1 and 100 characters."
-  end
 end
 
 # Create a new list
@@ -93,14 +110,14 @@ end
 # View a single todo list
 get "/lists/:id" do
   @list_id = params[:id].to_i
-  @list = session[:lists][@list_id]
+  @list = load_list(@list_id)
   erb :list, layout: :layout
 end
 
 # Edit an existing todo list
 get "/lists/:id/edit" do
   @id = params[:id].to_i
-  @list = session[:lists][@id]
+  @list = load_list(@list_id)
   erb :edit_list, layout: :layout
 end
 
@@ -108,7 +125,7 @@ end
 post "/lists/:id" do
   list_name = params[:list_name].strip
   @id = params[:id].to_i
-  @list = session[:lists][@id]
+  @list = load_list(@list_id)
 
   error = error_for_list_name(list_name)
   if error
@@ -135,7 +152,7 @@ end
 # Add a new todo to a list
 post "/lists/:list_id/todos" do
   @list_id = params[:list_id].to_i
-  @list = session[:lists][@list_id]
+  @list = load_list(@list_id)
   text = params[:todo].strip
 
   error = error_for_todo(text)
@@ -153,7 +170,7 @@ end
 post "/lists/:list_id/todos/:todo_id/delete" do
   @list_id = params[:list_id].to_i
   @todo_id = params[:todo_id].to_i
-  @list = session[:lists][@list_id]
+  @list = load_list(@list_id)
 
   todo_name = @list[:todos][@todo_id][:name]
   session[:success] = "Todo \"#{todo_name}\" has been deleted."
@@ -166,7 +183,7 @@ end
 post "/lists/:list_id/todos/:id" do
   @list_id = params[:list_id].to_i
   @todo_id = params[:id].to_i
-  @list = session[:lists][@list_id]
+  @list = load_list(@list_id)
 
   todo_name = @list[:todos][@todo_id][:name]
   session[:success] = "Todo \"#{todo_name}\" has been updated."
@@ -180,7 +197,7 @@ end
 # Mark all todos as complete for a list
 post "/lists/:id/complete_all" do
   @list_id = params[:id].to_i
-  @list = session[:lists][@list_id]
+  @list = load_list(@list_id)
   list_name = @list[:name]
 
   session[:success] = "All todos in list \"#{list_name}\" have been completed."
